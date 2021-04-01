e64f492d88590240b2babc90fffbf6f01f84c9e4
AFzal <saiyedafzalgz@gmail.com>
2021-04-01 02:33:11 -0700

    backed up database data in json_data folder

--- backend/settings.py
+++ backend/settings.py
@@ -162,8 +162,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
 }
 
 AUTH_USER_MODEL = 'account.CustomUser'
