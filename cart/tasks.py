789c49377d0e510edaf8ba855d3503fdbcb6b53a
AFzal <saiyedafzalgz@gmail.com>
2021-05-20 08:37:22 -0700

    added logo to email Template

--- cart/tasks.py
+++ cart/tasks.py
@@ -4,14 +4,15 @@ from .models import Order
 from api.models import Service
 from django.db.models import Case, When
 from django.core.mail import EmailMessage
-
+from django.contrib.staticfiles import finders
 @shared_task
 def order_success_mail(order_id, recommanded_ids):
     order = Order.objects.select_related('user').get(receipt=order_id)
     preserve_ids = Case(*[When(id=id, then=index) for index, id in enumerate(recommanded_ids)])
     services = Service.objects.filter(id__in=recommanded_ids).order_by(preserve_ids)
     subject = f"Order {order_id}"
-    body = render_to_string('emails/order_success.html', {'order': order, "services": services})
+    logo_path = finders.find('images/corecare_vertical_blue.png')
+    body = render_to_string('emails/order_success.html', {'order': order, "services": services, 'logo': logo_path})
     mail = EmailMessage(subject=subject, body=body, from_email="saiyedafzal0@gmail.com", to=[order.user.email])
     mail.content_subtype = "html"
     mail.mixed_subtype = "related"
