80572424fabf9271ba2600d029a7209110246123
afzal <saiyedafzalgz@gmail.com>
2021-05-21 13:19:59 -0700

    added easyprint to generate invoices on successful order

--- api/tasks.py
+++ api/tasks.py
@@ -1,27 +1,27 @@
 from celery import shared_task
-from django.core.mail import send_mail, EmailMessage
+from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 
 
-@shared_task
-def send_contact_or_partners_mail(type, data, _from, _to):
-    if type == "contact":
-        mail_subject = "CoreCare Contact Us"
-        mail_message = f"""A Contact Message from
-        Name: {data['first_name']} {data['last_name']}
-        email : {data['email']}
-        message: {data['message']}"""
-    elif type == "partner":
-        mail_subject = "CoreCare Partner Request"
-        mail_message = f"""
-        Name: {data['name']}
-        Number: {data['number']}
-        email : {data['email']}
-        detail: {data['detail']}"""
-    send_mail(mail_subject, mail_message,_from,_to)
+# @shared_task
+# def send_contact_or_partners_mail(type, data, _from, _to):
+#     if type == "contact":
+#         mail_subject = "CoreCare Contact Us"
+#         mail_message = f"""A Contact Message from
+#         Name: {data['first_name']} {data['last_name']}
+#         email : {data['email']}
+#         message: {data['message']}"""
+#     elif type == "partner":
+#         mail_subject = "CoreCare Partner Request"
+#         mail_message = f"""
+#         Name: {data['name']}
+#         Number: {data['number']}
+#         email : {data['email']}
+#         detail: {data['detail']}"""
+#     send_mail(mail_subject, mail_message,_from,_to)
 
 @shared_task
-def send_contact_or_partners_mail_new(type, data, _from, _to):
+def send_contact_or_partners_mail(type, data, _from, _to):
     if type == "contact":
         mail_subject = "Corecare contact us"
         _from_name = f"{data['first_name']} {data['last_name']}"
--- cart/tasks.py
+++ cart/tasks.py
@@ -4,16 +4,23 @@ from .models import Order
 from api.models import Service
 from django.db.models import Case, When
 from django.core.mail import EmailMessage
-from django.contrib.staticfiles import finders
+import weasyprint
+from io import BytesIO
+from django.conf import settings
+import os
 @shared_task
 def order_success_mail(order_id, recommanded_ids):
     order = Order.objects.select_related('user').get(receipt=order_id)
     preserve_ids = Case(*[When(id=id, then=index) for index, id in enumerate(recommanded_ids)])
     services = Service.objects.filter(id__in=recommanded_ids).order_by(preserve_ids)
     subject = f"Order {order_id}"
-    logo_path = finders.find('images/corecare_vertical_blue.png')
-    body = render_to_string('emails/order_success.html', {'order': order, "services": services, 'logo': logo_path})
+    body = render_to_string('emails/order_success.html', {'order': order, "services": services})
     mail = EmailMessage(subject=subject, body=body, from_email="saiyedafzal0@gmail.com", to=[order.user.email])
     mail.content_subtype = "html"
     mail.mixed_subtype = "related"
+    pdf_str = render_to_string('invoice/invoice.html', {"order": order})
+    empty_obj = BytesIO()
+    stylesheet = [weasyprint.CSS(settings.BASE_DIR / "staticfiles/static/css/main.css")]
+    weasyprint.HTML(string=pdf_str).write_pdf(target=empty_obj, stylesheets=stylesheet)
+    mail.attach(f"{order_id}.pdf", empty_obj.getvalue(), 'application/pdf')
     mail.send()
