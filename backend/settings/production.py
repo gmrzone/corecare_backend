e7cc3e41604cbcc83bc68249127d298b979c34c5
afzal <saiyedafzalgz@gmail.com>
2021-05-21 02:41:21 -0700

    changed Production settings and improved Order success Template

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -20,17 +20,7 @@ sentry_sdk.init(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-# POSTGRESS PRODUCTION
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ["PG_PROD_DB_NAME"],
-#         'USER': os.environ["PG_PROD_DB_USER"],
-#         'PASSWORD': os.environ["PG_PROD_DB_PASSWORD"],
-#         'HOST': os.environ["PG_PROD_DB_HOST"],
-#         'PORT': '5432',
-#     }
-# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -45,22 +35,16 @@ ADMINS = (('Afzal', 'saiyedafzalgz@gmail.com'), ('afzal1', 'saiyedafzalaz@gmail.
 
 
 # # HTTPS Settings
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 
 # # HSTS SETTINGS
-# SECURE_HSTS_SECONDS = 31536000 # 1 year
-# SECURE_HSTS_PRELOAD = True
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = 31536000 # 1 year
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'redis_cache.RedisCache',
-#         'LOCATION': os.environ["REDIS_LOCATION_AUTH"],	
-#         'TIMEOUT': 600
-#     },
-# }
+
 CACHES = {
     'default': {
         'BACKEND': 'redis_cache.RedisCache',
