23958ac6990026df6e971877e3e743156b9d90c9
afzal <saiyedafzalgz@gmail.com>
2021-06-03 07:06:50 -0700

    made pagination page_size dynamic nased on query

--- blog/pagination.py
+++ blog/pagination.py
@@ -2,6 +2,12 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class PostListPagination(PageNumberPagination):
+    def get_page_size(self, request):
+        size = request.query_params.get('size')
+        if size:
+            return size
+        return super().get_page_size(request)
+
     def get_paginated_response(self, data):
 
         return Response({
